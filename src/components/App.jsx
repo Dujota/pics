bb29715d507d90b56b4b9ec3e715b349fff712ff
Denis <ddujota@internationalsafety.com>
2018-12-15 11:32:30 -0500

    added axios and started the network call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,16 @@ import axios from 'axios';
 
 // Components
 import SearchBar from './SearchBar';
+import Axios from 'axios';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    // receives 2 arguments url string and the options object
+    // Add authorization on the header object
+    axios.get('https://api.unsplash.com/search', {
+      Authorization:
+        '5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
+    });
   }
 
   render() {
