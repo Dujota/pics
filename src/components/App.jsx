d5fb20f33181fde3316d056ef3224ce249e04f17
Denis <ddujota@internationalsafety.com>
2018-12-15 13:20:25 -0500

    completed the promise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,17 @@ class App extends React.Component {
   onSearchSubmit(term) {
     // receives 2 arguments url string and the options object
     // Add authorization on the header options object
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term }, // forms the query string for endpoint
-      headers: {
-        Authorization:
-          'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
-      }
-    });
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term }, // forms the query string for endpoint
+        headers: {
+          Authorization:
+            'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
