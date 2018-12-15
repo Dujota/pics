48d90975ab818bd38782b3975229eb491bc2c41b
Denis <ddujota@internationalsafety.com>
2018-12-15 15:23:22 -0500

    destructuring the properties of the image object out so we can use short form rference in jsx returned

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const ImageList = props => {
-  const images = props.images.map(image => {
-    return (
-      <img src={image.urls.regular} alt={image.description} key={image.id} />
-    );
+  // use destructuring to only pull the properties from the arg of the map which happens to be an obj passed in from the api call
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img src={urls.regular} alt={description} key={id} />;
   });
   return <div>{images}</div>;
 };
