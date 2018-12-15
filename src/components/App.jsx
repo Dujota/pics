382c92dca625bb35f1e39cc2080ec2ead0df14b2
Denis <ddujota@internationalsafety.com>
2018-12-15 13:44:58 -0500

    regactoed the promise into async await and set state to the response  and initialzed state objt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,26 +5,30 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  state = {
+    images: []
+  };
+
+  async onSearchSubmit(term) {
     // receives 2 arguments url string and the options object
     // Add authorization on the header options object
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term }, // forms the query string for endpoint
-        headers: {
-          Authorization:
-            'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term }, // forms the query string for endpoint
+      headers: {
+        Authorization:
+          'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
+      }
+    });
+
+    console.log(this);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
