0071b40e982d5c4a68b416f5e7f3e815090909e9
Denis <ddujota@internationalsafety.com>
2018-12-15 13:08:17 -0500

    fixed options object error and can now send query with 200 to unsplash api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,17 @@ import axios from 'axios';
 
 // Components
 import SearchBar from './SearchBar';
-import Axios from 'axios';
 
 class App extends React.Component {
   onSearchSubmit(term) {
     // receives 2 arguments url string and the options object
-    // Add authorization on the header object
-    axios.get('https://api.unsplash.com/search', {
-      Authorization:
-        '5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
+    // Add authorization on the header options object
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
+      }
     });
   }
 
