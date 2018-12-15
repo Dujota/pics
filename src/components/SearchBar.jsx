e8f74540bfb67df6cb18f9dd7314100a0025cdb8
Denis <ddujota@internationalsafety.com>
2018-12-15 00:06:43 -0500

    fixed the undefined this fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' }; // initialize a state variable and set it to empty string/value
 
+  onFormSubmit = e => {
+    e.preventDefault();
+    console.log(this);
+    console.log(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
