59ae01f3c9102112f5d54eb0b6b0b4bea06bc171
Denis <ddujota@internationalsafety.com>
2018-12-15 14:13:29 -0500

    refactored by creating a preconfigured unsplash axios obj and then passing that to react to make the call

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,3 +1,12 @@
 import axios from 'axios';
 
-axios.create({});
+// sets up a pre-configured instance of the axios client that has default properties set like url, headers and params
+
+//makes an instance of client with some default properties, customized to make a specific request for a given url
+export default axios.create({
+  baseURL: 'https://api.unsplash.com',
+  headers: {
+    Authorization:
+      'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
+  }
+});
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 
 // Components
 import SearchBar from './SearchBar';
@@ -12,12 +12,8 @@ class App extends React.Component {
   onSearchSubmit = async term => {
     // receives 2 arguments url string and the options object
     // Add authorization on the header options object
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term }, // forms the query string for endpoint
-      headers: {
-        Authorization:
-          'Client-ID 5190bd906111c2df1da8575f0e44be267f79c1c479e90c70f3bd9a0bd9d75c5d'
-      }
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term } // forms the query string for endpoint
     });
 
     this.setState({ images: response.data.results });
