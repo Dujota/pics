687c7ee0159a46f6868dd6ca3c006ab5f5550b34
Denis <ddujota@internationalsafety.com>
2018-12-16 16:04:00 -0500

    added the ref to constructor and added to the dom via img tag prop

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.imageRef is the instance variable
+    this.imageRef = React.createRef();
+  }
+
   render() {
     const { urls, description } = this.props.image;
     return (
       <div>
-        <img src={urls.regular} alt={description} />
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
